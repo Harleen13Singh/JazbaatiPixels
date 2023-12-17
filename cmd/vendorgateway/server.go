7b54918e58a51c8c3e2c416b2468989b4234a5cb
Harleen Singh <harleensingh@Harleens-MacBook-Pro.local>
2023-12-17 16:49:03 +0530

    chore: reflections added in the server

--- cmd/vendorgateway/server.go
+++ cmd/vendorgateway/server.go
@@ -5,6 +5,7 @@ import (
 	"net"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 
 	vgPb "github.com/personal/jazbaatipixels/api/vendorgateway"
 	vg "github.com/personal/jazbaatipixels/vendorgateway"
@@ -16,7 +17,7 @@ func main() {
 		log.Panic("failed to listen,err: %w", err)
 	}
 	grpcServer := grpc.NewServer()
-
+	reflection.RegisterV1(grpcServer)
 	vgSvc := vg.NewService()
 	vgPb.RegisterVendorGatewayServer(grpcServer, vgSvc)
 	serveErr := grpcServer.Serve(list)
